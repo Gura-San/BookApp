87b7f0c7dffec124cc2647968bda5f40fa7f557a
Gura-San <gura.san@gmail.com>
2018-03-06 14:49:58 -0500

    added category dropdown when adding book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306183838) do
+ActiveRecord::Schema.define(version: 20180306193403) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180306183838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "installs", force: :cascade do |t|
